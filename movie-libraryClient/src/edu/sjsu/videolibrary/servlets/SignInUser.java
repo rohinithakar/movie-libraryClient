861b4c5e79e642589f2fcc9de36568386d5fc27d
fredhttp <fredhttp@gmail.com>
2012-11-18 13:35:13 -0800

    Update movie-libraryClient/src/edu/sjsu/videolibrary/servlets/SignInUser.java

--- movie-libraryClient/src/edu/sjsu/videolibrary/servlets/SignInUser.java
+++ movie-libraryClient/src/edu/sjsu/videolibrary/servlets/SignInUser.java
@@ -14,7 +14,7 @@ import Connection.ServicesProxy;
 /**
  * Servlet implementation class SignInUser
  */
-@WebServlet("/SignInUser")
+
 public class SignInUser extends HttpServlet 
 {
 	private static final long serialVersionUID = 1L;
